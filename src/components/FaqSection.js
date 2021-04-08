ee3e6283bf59c77f9875c690984023427ea84784
adelevrc <adele.vercaygne@gmail.com>
2021-04-08 18:22:05 +0200

    Faq section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,40 @@
+import React from 'react'; 
+
+const FaqSection = () => {
+    return(
+        <div className="faq">
+            <h2> Any questions ? <span> FAQs</span></h2>
+            <div className="question">
+                <h4> how do I start ? </h4>
+            </div>
+            <div className="answer">
+                <p> Lorem ipsum dolor sit amet.</p>
+                <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores, placeat!s</p>
+            </div>
+            <div className="question">
+                <h4> Daily schedule</h4>
+            </div>
+            <div className="answer">
+                <p> Lorem ipsum dolor sit amet.</p>
+                <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores, placeat!s</p>
+            </div>
+            <div className="question">
+                <h4> Different Payment Methods </h4>
+            </div>
+            <div className="answer">
+                <p> Lorem ipsum dolor sit amet.</p>
+                <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores, placeat!s</p>
+            </div>
+            <div className="question">
+                <h4> What product do we offer ?  </h4>
+            </div>
+            <div className="answer">
+                <p> Lorem ipsum dolor sit amet.</p>
+                <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores, placeat!s</p>
+            </div>
+        </div>
+
+    )
+
+}
+export default FaqSection;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,6 +1,7 @@
 import React from 'react'; 
 import AboutSection from '../components/AboutSection'; 
 import ServicesSection from '../components/ServicesSection'; 
+import FaqSection from '../components/FaqSection'; 
 
 const AboutUs = () => {
     return(
@@ -8,6 +9,7 @@ const AboutUs = () => {
         <div>
             <AboutSection />
             <ServicesSection/>
+            <FaqSection />
         </div>
     </>
     )
