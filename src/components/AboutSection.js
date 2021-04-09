a02333619e6808622c6146653ab5482c73bf559e
adelevrc <adele.vercaygne@gmail.com>
2021-04-09 17:45:11 +0200

    styling services section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,59 +1,39 @@
-import React from 'react'; 
-import home1 from '../img/home1.png'; 
-import styled from 'styled-components'; 
+import React from "react";
+import home1 from "../img/home1.png";
+//Styled
+import styled from "styled-components";
+import { About, Description, Image, Hide } from "../styles";
 
 const AboutSection = () => {
-    return(
-        <About>
-            <Description>
-                <div className="title">
-                    <Hide>
-                        <h2>We work to make </h2> 
-                    </Hide>
-                    <Hide>
-                        <h2> Your <span> dreams</span> come </h2>
-                    </Hide>
-                    <Hide>
-                        <h2> true. </h2>
-                    </Hide>
-                    <p> Contact us for any photography or videography ideas that you have. We have professionnals with amazing skills</p>
-                <   button> Contact Us </button>
-                </div>
-                </Description>
-                <Image>
-                    <img src={home1} alt="guy with a camera"/>
-                </Image>
-        </About>
-    )
-}
-const About = styled.div`
-    min-height: 90vh; 
-    display:flex;
-    align-items:center; 
-    justify-content:space-between; 
-    padding:5rem 10rem; 
-    color:white; 
-`; 
+  return (
+    <About>
+      <Description>
+        <div className="title">
+          <Hide>
+            <h2>We work to make</h2>
+          </Hide>
+          <Hide>
+            <h2>
+              your <span>dreams</span> come
+            </h2>
+          </Hide>
+          <Hide>
+            <h2>true.</h2>
+          </Hide>
+        </div>
+        <p>
+          Contact us for any photography or videography ideas that you have. We
+          have professionals with amazing skills.
+        </p>
+        <button>Contact Us</button>
+      </Description>
+      <Image>
+        <img src={home1} alt="guy with a camera" />
+      </Image>
+    </About>
+  );
+};
 
-const Description = styled.div`
-    flex:1; 
-    padding-right:5rem; 
-    h2{
-        font-weight:lighter; 
-    }
-`; 
+//Styled Components
 
-const Image = styled.div`
-    flex:1;
-    overflow:hidden; 
-    img{
-        width:100%;
-        height: 80vh; 
-        object-fit: cover; 
-    }
-`; 
-
-const Hide = styled.div`
-overflow:hidden; `
-
-export default AboutSection; 
+export default AboutSection;
