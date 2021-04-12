e013cefed768e523f28c69b69ab743192a557043
adelevrc <adele.vercaygne@gmail.com>
2021-04-12 16:41:45 +0200

    configure react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from 'react'; 
+import {Switch, Route} from 'react-router-dom'; 
 import Nav from './components/Nav'; 
 import AboutUs from './pages/AboutUs'; 
 import GlobalStyle from './components/GlobalStyle'; 
+import ContactUs from './pages/ContactUs'
+import OurWork from './pages/OurWork'; 
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+
+      <Switch>
+      <Route path="/" exact>
+        <AboutUs />
+      </Route>
+
+      <Route path="/work">
+        <OurWork />      
+      </Route>
+
+      <Route path="/contact">
+        <ContactUs />
+      </Route>
+      </Switch>
+     
+      
     </div>
   );
 }
