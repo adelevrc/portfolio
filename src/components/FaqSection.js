06b6933602a96edbd6c366d9131c3da5a1028205
adelevrc <adele.vercaygne@gmail.com>
2021-04-13 14:35:55 +0200

    scroll animation + style contact page

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -2,13 +2,16 @@ import React, {useState} from 'react';
 import styled from "styled-components";
 import { About } from "../styles";
 import Toggle from './Toggle';
-import { AnimateSharedLayout } from 'framer-motion' 
+import { AnimateSharedLayout } from 'framer-motion' ; 
+import { useScroll } from './useScroll'; 
+import { scrollReveal } from '../animation'; 
 
 
 const FaqSection = () => {
+    const [element, controls] = useScroll(); 
  
     return(
-        <Faq>
+        <Faq variants={scrollReveal} ref={element} animate={controls} initial="hidden">
             <h2> 
                 Any questions ? <span> FAQs</span>
             </h2>
--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -7,8 +7,12 @@ import goodtimes from '../img/goodtimes-small.png';
 
 import {motion} from 'framer-motion'; 
 import {sliderContainer, slider, pageAnimation,fade, photoAnim, lineAnim} from '../animation'; 
+import {useScroll} from '../components/useScroll'; 
+
 
 const OurWork = () => {
+    const [element, controls] = useScroll(); 
+    const [element2, controls2] = useScroll(); 
     return(
         <Work 
             exit="exit" 
@@ -34,16 +38,24 @@ const OurWork = () => {
                     </Hide>
                 </Link>
             </Movie>
-            <Movie>
+
+            <Movie ref={element} variants={fade} animate={controls} initial="hidden">
                 <h2>The Racer </h2> 
-                <div className="line"></div>
+                <motion.div variants={lineAnim} className="line"></motion.div>
                 <Link to="/work/the-racer">  
                     <img src={theracer} alt="the racer" />
                 </Link>
             </Movie>
-            <Movie>
+
+            <Movie 
+                ref={element2} 
+                variants={fade} 
+                animate={controls2} 
+                initial="hidden"
+                >
+                    
                 <h2>Good Times </h2> 
-                <div className="line"></div>
+                <motion.div variants={lineAnim} className="line"></motion.div>
                 <Link to="/work/good-times"> 
                     <img src={goodtimes} alt="good times" />
                 </Link>
@@ -62,7 +74,7 @@ const Work = styled(motion.div)`
     }
 `; 
 
-const Movie = styled.div`
+const Movie = styled(motion.div)`
 padding-bottom: 10rem; 
 .line{
     height:0.5rem; 
